b3eed7bc56e9c0ec41e12a12636dae93c7c278d1
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-09-30 19:46:40 +0300

    Perform the state in App and Main.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/main_componnets/Main"
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
+  const [score, setScore] = useState(0);
+
   return (
     <>
-      <Header />
-      <Main />
+      <Header score={score} />
+      <Main score={score} setScore={setScore} />
       <Footer />
     </>
   );
--- src/components/main_componnets/Main.jsx
+++ src/components/main_componnets/Main.jsx
@@ -2,11 +2,19 @@ import { useState, useEffect } from "react";
 import Board from "./Board";
 import Msg from "./Msg";
 
-const Main = () => {
+const instruction =
+  "You can click each Pokémon only once. You will lose if you click the same Pokémon twice.";
+
+const Main = ({ score, setScore }) => {
+  const [msg, setMsg] = useState({
+    type: "info",
+    text: instruction,
+  });
+
   return (
     <main>
-      <Msg />
-      <Board />
+      <Msg msg={msg} />
+      <Board score={score} setScore={setScore} setMsg={setMsg} />
     </main>
   );
 };
